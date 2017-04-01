d37cac3c68a6ec0a603fa7054770fc7cffaff6b5
Amen Moja Ra <amenra@Amens-Mac-mini.local>
2017-03-31 21:55:42 -0400

    module 3 code and final code for the project

---
+++ examples/module 3/sub module 1/main-1.ts
@@ -0,0 +1,39 @@
+import { Observable } from "rxjs";
+import { loadWithFetch, load} from './loader';
+
+let source = Observable.create(observer => {
+    observer.next(1);
+    observer.next(2);
+    observer.error("Stop!");
+    //throw new Error("Stop");
+    observer.next(3);
+    observer.complete();
+});
+
+source.subscribe(
+    value => console.log(`value: ${value}`),
+    error => console.log(`error ${error}`),
+    () => console.log("complete")
+);
+
+// let output = document.getElementById("output");
+// let button = document.getElementById("button");
+
+// let click = Observable.fromEvent(button, "click");
+
+// function renderMovies(movies) {
+//     movies.forEach(m => {
+//         let div = document.createElement("div");
+//         div.innerText = m.title;
+//         output.appendChild(div);
+//     })
+// }
+
+// loadWithFetch("movies.json").subscribe(renderMovies);
+
+// click.flatMap(e => loadWithFetch("movies.json"))
+//     .subscribe(
+//     renderMovies,
+//     e => console.log(`error: ${e}`),
+//     () => console.log("complete")
+//     );
